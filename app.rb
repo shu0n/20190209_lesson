460c687f970e0ae2705064178a48ba3ebd3207da
shu0n <venividivici974@gmail.com>
2019-02-09 10:55:00 +0000

    ゲットとポストの追加

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ require 'sinatra/reloader'
 register Sinatra::Reloader
 
 get '/' do
+  p request.env
   "hello, world!!!\n"
+end
+
+get '/hoge' do
+  name = params["name"]
+  "My name is #{name}.\n"
+end
+
+post '/hoge' do
+  name = params["name"]
+  "My name is #{name}.\n"
 end
