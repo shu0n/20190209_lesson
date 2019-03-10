95fb7179696d872b7bcbd4426e33f6493af77d88
shu0n <venividivici974@gmail.com>
2019-03-10 15:12:46 +0000

    じゃんけんを作った

--- app.rb
+++ app.rb
@@ -17,3 +17,17 @@ post '/hoge' do
   name = params["name"]
   "My name is #{name}.\n"
 end
+
+get '/janken' do
+  userHand = params["hand"]
+  compHands = ["guu", "choki", "paa"]
+  compHand = compHands.sample
+ 
+  if (userHand == "guu" && compHand == "choki") || (userHand == "choki" && compHand == "paa") || (userHand == "paa" && compHand == "guu")
+    "I am #{compHand}. You are #{userHand}. You win.\n"
+  elsif (userHand == "guu" && compHand == "paa") || (userHand == "choki" && compHand == "guu") || (userHand == "paa" && compHand == "choki")
+    "I am #{compHand}. You are #{userHand}. You lose.\n"
+  else
+    "Draw!"
+  end
+end
